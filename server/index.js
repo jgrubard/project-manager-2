3198a88a23b3645854e941a481c2431b1ebeafcf
Jeremy Grubard <jgrubard@gmail.com>
2018-11-25 20:00:35 -0500

    webpack and react wired up on the front end

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const db = require('./db');
+const path = require('path');
 const port = process.env.PORT || 3000;
 
-const db = require('./db');
+app.use('/dist', express.static(path.join(__dirname, '../dist')));
+
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+});
 
 app.listen(port, () => console.log(`** PORT ${port} **`));
 
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const config = {
+  devtool: 'source-maps',
+  entry: './client/src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: [ 'es2015', 'react']
+        }
+      }
+    ]
+  }
+}
+
+module.exports = config;
